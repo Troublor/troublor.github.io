79381876234a0ddacb4ddf82db65a286514e1f49
GitHub Action <Troublor@users.noreply.github.com>
2024-09-30 01:27:05 +0000

    changes by format script

--- src/home/data/misc-data.ts
+++ src/home/data/misc-data.ts
@@ -91,7 +91,7 @@ export default <MiscData>{
     'Zhuo Zhang': 'https://zzhang.xyz/',
     'Qingkai Shi': 'https://qingkaishi.github.io/',
     'Xiangyu Zhang': 'https://www.cs.purdue.edu/homes/xyzhang/',
-    'MegaETH': 'https://megaeth.systems/',
+    MegaETH: 'https://megaeth.systems/',
     'Chengpeng Wang': 'https://chengpeng-wang.github.io',
     'Guannan Wei': 'https://continuation.passing.style/',
     'Danning Xie': 'https://dnxie.github.io/',
